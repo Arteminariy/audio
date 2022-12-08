62d4e874b095effd21526e6bcd3a7204bd57d452
Arteminariy <artemborisoff.ab@gmail.com>
2022-12-08 21:35:26 +0300

    start

---
+++ templates/default/TemplateName.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TemplateName.module.css';
+
+interface TemplateNameProps { }
+
+const TemplateName = ({ }: TemplateNameProps) => {
+	return (
+		<div className='templateName'>
+			TemplateName Component
+		</div>
+	)
+};
+
+export default TemplateName;
